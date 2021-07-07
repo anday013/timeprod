901f01471e7c065b7eb338dff5099d710884cceb
Anday <48630069+anday2000@users.noreply.github.com>
2021-07-07 11:35:21 +0400

    Initial Commit

---
+++ TimePad/TimePadApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimePadApp.swift
+//  TimePad
+//
+//  Created by Anday on 07.07.21.
+//
+
+import SwiftUI
+
+@main
+struct TimePadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
